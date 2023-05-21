7bdee26a5ea6da043c59ea2dc5a721be0b0679d6
minjucse <minjucse@gmail.com>
2023-05-21 23:42:16 +0600

    blog page added

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -4,7 +4,43 @@ import useTitle from '../../hooks/useTitle';
 const Blog = () => {
   useTitle('Blogs');
   return (
-    <div>Blog</div>
+    <div className="carousel w-[80%] h-[550px] mx-auto">
+      <div className=" card mb-4">
+      <div className="mb-4">
+            <h1 className="font-bold mb-2">
+              1. What is an access token and refresh token? How do they work and where should we store them on the client-side?.
+            </h1>
+            <p>
+              <strong>Answer</strong>: Storing tokens in browser local storage provides persistence across page refreshes and browser tabs.
+            </p>
+          </div>
+          <div className="mb-4">
+            <h1 className="font-bold mb-2">2. Compare SQL and NoSQL databases?
+
+            </h1>
+            <p>
+              <strong>Answer</strong>: SQL databases are primarily called Relational Databases (RDBMS).These databases have fixed or static or predefined schema. NoSQL databases are
+              Non-relational or distributed database system.They have a dynamic schema.
+            </p>
+          </div>
+          <div className="mb-4">
+            <h1 className="font-bold mb-2">
+              3. What is express js? What is Nest JS?
+            </h1>
+            <p>
+              <strong>Answer</strong> : 
+              Express.js is a framework based on Node.js which is used for building web-application using approaches and principles of Node.js event-driven architecture.
+              NestJS is a framework for building efficient, scalable Node.js web applications.
+            </p>
+          </div>
+          <div className="mb-4">
+            <h1 className="font-bold mb-2">4. What is MongoDB aggregate and how does it work?</h1>
+            <p>
+              <strong>Answer</strong> : The aggregation pipeline allows you to perform complex operations that will allow any range of insights into your collections. . 
+            </p>
+          </div>
+      </div>
+    </div>
   )
 }
 
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,17 +1,109 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Banner from '../Banner/Banner'
 import useTitle from '../../../hooks/useTitle';
+import Rating from 'react-rating';
+import { FaStar, FaRegStar } from 'react-icons/fa';
 
 const Home = () => {
     useTitle('Home');
+    const [activeTab, setActiveTab] = useState("all");
+    const [products, setProduct] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://server-phi-sooty.vercel.app/productByCategory/${activeTab}`)
+            .then((res) => res.json())
+            .then((result) => {
+                setProduct(result);
+            });
+    }, [activeTab]);
+
+    const handleTabClick = (tabName) => {
+        setActiveTab(tabName);
+    };
     return (
         <div>
             <Banner />
-            <div className="tabs tabs-boxed">
-                <a className="tab tab-active">All</a>
-                <a className="tab ">Tab 2</a>
-                <a className="tab">Tab 3</a>
+            <div className='category-area mt-8  w-[80%] mx-auto'>
+                <div className=" heading-section text-center  mb-12 mt-12">
+                    <h2 className="text-3xl text-center font-bold">OUR Category</h2>
+                    <p className='heading-description'>Explore thousands of recipe opportunities with all the information you need</p>
+                </div>
+                <div className="tabs flex justify-center items-center mb-12 mt-12">
+                    <div className="btn-group btn-group-horizontal">
+                        <button className={`${activeTab == "all" ? " btn btn-active" : "btn btn-ghost"
+                            }`} onClick={() => handleTabClick("all")}>All</button>
+                        <button className={`${activeTab == "Mickey Mouse" ? " btn btn-active" : "btn btn-ghost"
+                            }`} onClick={() => handleTabClick("Mickey Mouse")}>Mickey Mouse</button>
+                        <button className={`${activeTab == "Robot" ? " btn btn-active" : "btn btn-ghost"
+                            }`} onClick={() => handleTabClick("Robot")}>Robot</button>
+                        <button className={`${activeTab == "Monster" ? " btn btn-active" : "btn btn-ghost"
+                            }`} onClick={() => handleTabClick("Monster")}>Monster</button>
+                    </div>
+
+                </div>
+                <div className='single-featured'>
+                    <div className='grid grid-cols-1 md:grid-cols-4 gap-4'>
+                        {
+                            products?.map((item, index) => (
+                                <div key={index}>
+                                    <div className="card w-96 bg-base-100 shadow-xl" >
+                                        <figure><img className='h-[250px]' src={item.imageURL} alt="Shoes" /></figure>
+                                        <div className="card-body">
+                                            <h2 className="card-title">
+                                                {item.name}!
+                                            </h2>
+                                            <p>
+                                                {item.price}!
+                                            </p>
+                                            <div className='mb-4'>
+                                                <Rating
+                                                    placeholderRating={item.rating} readonly
+                                                    emptySymbol={<FaRegStar></FaRegStar>}
+                                                    placeholderSymbol={<FaStar className='text-warning'></FaStar>}
+                                                    fullSymbol={<FaStar></FaStar>}
+                                                >
+                                                </Rating>
+
+                                                <span className='ms-2'> {item?.rating}</span>
+                                            </div>
+                                            <div className="card-actions justify-end">
+                                                <Link className='badge badge-outline' to={`/product-detail/${item._id}`}> Details</Link>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                    </div>
+
+                </div>
             </div>
+            <div className='famous-area mt-8  w-[80%] mx-auto'>
+                <div className=" heading-section text-center  mb-12 mt-12">
+                    <h2 className="text-3xl text-center font-bold">OUR Famous Toys</h2>
+                    <p className='heading-description'>Explore thousands of recipe opportunities with all the information you need</p>
+                </div>
+
+                <div className='single-featured-famous'>
+                    <div className='grid grid-cols-1 md:grid-cols-4 gap-4'>
+                        <div className='famous-img'>
+                            <img className='w-[100%] h-[250px]' src="https://images.unsplash.com/photo-1612618558821-40f47381f5b8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=433&q=80" alt="" />
+                        </div>
+                        <div className='famous-img'>
+                            <img className='w-[100%] h-[250px]' src="https://images.unsplash.com/photo-1641302109103-912af938b5a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="" />
+                        </div>
+                        <div  className='famous-img'>
+                            <img className='w-[100%] h-[250px]' src="https://images.unsplash.com/photo-1581909199603-0de32b43ffd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="" />
+                        </div>
+                        <div className='famous-img'>
+                            <img className='w-[100%] h-[250px]' src="https://images.unsplash.com/photo-1585621374760-213927cbfac7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1217&q=80" alt="" />
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     )
 }
